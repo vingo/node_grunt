74a181b9394b61f6437255d978a02bf99d0c9cd2
vingo <maoobo@163.com>
2016-04-08 11:16:36 +0800

    add package concurrent,babel

---
+++ es5/app.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var _marked = [test].map(regeneratorRuntime.mark);
+
+var arr = ['ax', 'bcc', 'c'];
+var l = arr.map(function (s) {
+    return s.length;
+});
+var k = arr.map(function (s) {
+    return s.length;
+});
+console.log(l, k, 'modify content*****,modify2');
+var s = 'the content is template :' + k;
+
+function test() {
+    return regeneratorRuntime.wrap(function test$(_context) {
+        while (1) {
+            switch (_context.prev = _context.next) {
+                case 0:
+                    _context.next = 2;
+                    return 1;
+
+                case 2:
+                case 'end':
+                    return _context.stop();
+            }
+        }
+    }, _marked[0], this);
+}
+module.exports = count = k;
--- src/app.js
+++ src/app.js
@@ -3,4 +3,11 @@ var arr = ['ax', 'bcc', 'c'];
 var l = arr.map(function(s) {
     return s.length;
 });
-console.log(l, 'modify content*****');
+var k = arr.map(s => s.length);
+console.log(l, k, 'modify content*****,modify2');
+let s = `the content is template :${k}`;
+
+function* test() {
+    yield 1;
+}
+module.exports = count = k;
