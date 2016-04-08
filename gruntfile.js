214fddd6eb773882d7a4d60b63281c228f8270de
vingo <maoobo@163.com>
2016-04-08 11:17:43 +0800

    upgrade grunt file and package json file

--- gruntfile.js
+++ gruntfile.js
@@ -8,32 +8,55 @@ module.exports = function(grunt) {
         banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
       },
       build: {
-        src: './src/app.js',
+        src: './es5/app.js',
         dest: './build/app.min.js'
       }
     },
     watch: {
-      files: ['./src/app.js'],
-      tasks: ['uglify'] //执行压缩task文件变更后
+      generales5: {
+        files: ['./src/*'],
+        //tasks: ['babel', 'uglify'], //执行压缩task文件变更后
+        tasks: ['babel'] //编译为es5
+      },
+      compressEs5: {
+        files: ['./es5/*'],
+        tasks: ['uglify'] //执行压缩
+      }
+    },
+    babel: {
+      options: {
+        sourceMap: false, //生成源码文件
+        presets: ['es2015']
+      },
+      dist: {
+        files: {
+          './es5/app.js': './src/app.js' //dist:es5-->  src: es6
+        }
+      }
+    },
+    concurrent: {
+      task: ['uglify', 'watch', 'babel'],
+      options: {
+        logConcurrentOutput: true,
+        limit: 3
+      }
     }
   });
 
   // 加载任务的插件。
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-babel');
+  grunt.loadNpmTasks('grunt-concurrent');
   //grunt.loadNpmTasks('grunt-contrib-concat'); //文件合并
   // 默认被执行的任务列表。
-  grunt.registerTask('watch',
-    //['watch'],
-    function() {
-      var args = arguments;
-      if (args.length <= 0) {
-        console.log('the task no args');
-      } else {
-        console.log('ready to exec task:', args[0]);
-        grunt.task.run(['watch']);
-      }
+  grunt.option('force', true);
+  //grunt.registerTask('default', ['uglify', 'watch', 'babel']);
+  /*grunt.registerTask('test', function(taskName) {
+    console.log(taskName);
+    grunt.task.run(['watch:generales5']);
+    grunt.task.run(['watch:compressEs5']);
+  });*/
+  grunt.registerTask('default', ['concurrent']);
 
-    });
-  grunt.registerTask('default', ['uglify', 'watch']);
 };
